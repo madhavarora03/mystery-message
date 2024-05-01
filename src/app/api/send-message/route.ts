627144c85f03fcd25824b05aea4f78b20c53b390
madhav <eragon.cube@outlook.com>
2024-05-01 15:14:06 +0530

    Integrating openai model

--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -1,9 +1,5 @@
-import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/[...nextauth]/options';
 import { dbConnect } from '@/lib/dbConnect';
 import UserModel, { Message } from '@/models/User';
-import { User } from 'next-auth';
-import mongoose from 'mongoose';
 
 export async function POST(request: Request) {
   await dbConnect();
